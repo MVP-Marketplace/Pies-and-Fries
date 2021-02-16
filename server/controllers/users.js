c49bf518c278781726599df17f35ac6d2d7d77fe
DNiev <danny.nieves411@gmail.com>
2021-02-16 13:22:53 -0500

    added login and create user routes and controllers

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -5,7 +5,19 @@ exports.createUser = async (req, res) => {
     try {
         const user = new User({
             name,
-            email
-        })
+            email,
+            password
+        });
+        const token = await user.generateAuthToken();
+        res.cookie('jwt', token, {
+            httpOnly: true,
+            sameSite: 'Strict',
+            secure: process.env.NODE_ENV !== 'production' ? false:true
+        });
+        res.status(201).json(user);
+    } catch (e) {
+        res.status(400).json({ error: e.toString() });
     }
-}
+};
+
+exports.loginUser = async (req, res) => {}
