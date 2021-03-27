5edb770c1c49a2ff2a1948b8fc40e0f8271df298
Qui <quivan3396@yahoo.com>
2021-03-26 20:09:42 -0400

    feat: added a cart to navbar and created Profile page component

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,30 @@
 import './App.css';
-import {useState} from 'react'
-import { Route } from 'react-router-dom'
-import SignIn from './components/SignIn'
-import UserDashboard from './components/UserDashboard'
-import { AppContextProvider } from './context/AppContext'
-import Navbar from './components/Navbar'
-import Store from './components/Store'
-import Home from './pages/Home'
+import { useState } from 'react';
+import { Route } from 'react-router-dom';
+import SignIn from './components/SignIn';
+import UserDashboard from './components/UserDashboard';
+import { AppContextProvider } from './context/AppContext';
+import Navbar from './components/Navbar';
+import Store from './components/Store';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
 
 function App() {
-  const [userLoggedIn, setUserLogginIn] = useState(false)
+  const [userLoggedIn, setUserLogginIn] = useState(false);
   return (
     <>
-    <AppContextProvider>
-      <Navbar 
-      userLoggedIn={userLoggedIn} 
-      setUserLogginIn ={setUserLogginIn}/>
-      <Route exact path="/" component={Home}/>
-      <Route exact path="/signin" render={() => (
-        <SignIn 
-        signIn={true}
+      <AppContextProvider>
+        <Navbar userLoggedIn={userLoggedIn} setUserLogginIn={setUserLogginIn} />
+        <Route exact path='/' component={Home} />
+        <Route exact path='/signin' render={() => <SignIn signIn={true} />} />
+        <Route exact path='/signup' render={() => <SignIn signIn={false} />} />
+        <Route
+          exact
+          path='/dashboard'
+          render={() => <UserDashboard setUserLogginIn={setUserLogginIn} />}
         />
-      )} />
-      <Route exact path="/signup" render={() => (
-        <SignIn 
-        signIn={false}
-        />
-      )} />
-      <Route exact path="/dashboard" render={() => (
-        <UserDashboard 
-        setUserLogginIn ={setUserLogginIn}
-        />
-      )} />
-       <Route exact path="/store" render={() => (
-        <Store 
-        />
-      )} />
+        <Route exact path='/store' render={() => <Store />} />
+        <Route exact path='/profile' render={() => <Profile />} />
       </AppContextProvider>
     </>
   );
---
+++ client/src/pages/Profile.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Profile = () => {
+    return (
+        <div>
+    testing            
+        </div>
+    )
+}
+
+export default Profile
