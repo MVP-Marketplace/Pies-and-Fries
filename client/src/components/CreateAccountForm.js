5d69a186e34687339e3cb1822e1dc8e67fb9fe23
Jorge <jorgesilva1892@gmail.com>
2021-04-07 17:24:08 -0700

    css for login page

--- client/src/components/CreateAccountForm.js
+++ client/src/components/CreateAccountForm.js
@@ -1,21 +1,19 @@
-
-import {useState} from 'react'
+import { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 
- const CreateAccountForm = () => {
+const CreateAccountForm = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [number, setNumber] = useState('');
+  const [address, setAddress] = useState('');
+  const [passwordMatch, setPasswordMatch] = useState('');
 
-  const [name,setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [password,setPassword] = useState('')
-  const [number,setNumber] = useState('')
-  const [address,setAddress] = useState('')
-  const [passwordMatch, setPasswordMatch] = useState('')
-
-  const handleSubmit = (e) => {
-    if(passwordMatch === password ) {
-      e.preventDefault()
+  const handleSubmit = e => {
+    if (passwordMatch === password) {
+      e.preventDefault();
       fetch('/api/users', {
-        method: "POST",
+        method: 'POST',
         body: JSON.stringify({
           name: name,
           email: email,
@@ -24,58 +22,85 @@ import { Form, Button } from 'react-bootstrap';
           address: address,
         }),
         headers: {
-          "Content-Type": "application/json",
-        }
+          'Content-Type': 'application/json',
+        },
       })
-      .then((res) => res.json())
-      .catch((err) => {
-        console.log(err)
-      })
+        .then(res => res.json())
+        .catch(err => {
+          console.log(err);
+        });
     } else {
-      alert('passwords do not match')
+      alert('passwords do not match');
     }
-   
-  }
-    return (
-        <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="formBasicUserName">
+  };
+  return (
+    <>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId='formBasicUserName'>
           <Form.Label>Name</Form.Label>
-          <Form.Control type="Text" placeholder="EnterName" value = {name} onChange={(e) => setName(e.target.value)}/>
+          <Form.Control
+            type='Text'
+            placeholder='EnterName'
+            value={name}
+            onChange={e => setName(e.target.value)}
+          />
         </Form.Group>
 
-        <Form.Group controlId="formBasicEmail">
+        <Form.Group controlId='formBasicEmail'>
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" value = {email}  onChange={(e) => setEmail(e.target.value)}/>
-          <Form.Text className="text-muted">
+          <Form.Control
+            type='email'
+            placeholder='Enter email'
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+          <Form.Text className='text-muted'>
             We'll never share your email with anyone else.
           </Form.Text>
         </Form.Group>
-      
-        <Form.Group controlId="formBasicPassword">
+
+        <Form.Group controlId='formBasicPassword'>
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" value = {password} onChange={(e) => setPassword(e.target.value)}/>
+          <Form.Control
+            type='password'
+            placeholder='Password'
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
           <Form.Label>Confirm Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" value = {passwordMatch} onChange={(e) => setPasswordMatch(e.target.value)}/>
+          <Form.Control
+            type='password'
+            placeholder='Password'
+            value={passwordMatch}
+            onChange={e => setPasswordMatch(e.target.value)}
+          />
         </Form.Group>
 
-        <Form.Group controlId="formBasicPhoneNumber">
+        <Form.Group controlId='formBasicPhoneNumber'>
           <Form.Label>Phone Number</Form.Label>
-          <Form.Control type="number" placeholder="305-273-1232" value = {number} onChange={(e) => setNumber(e.target.value)}/>
+          <Form.Control
+            type='number'
+            placeholder='305-273-1232'
+            value={number}
+            onChange={e => setNumber(e.target.value)}
+          />
         </Form.Group>
-        <Form.Group controlId="formBasicAddress">
+        <Form.Group controlId='formBasicAddress'>
           <Form.Label>Address</Form.Label>
-          <Form.Control type="text" placeholder="123 Main Street, New York, NY 10030" value = {address} onChange={(e) => setAddress(e.target.value)}/>
+          <Form.Control
+            type='text'
+            placeholder='123 Main Street, New York, NY 10030'
+            value={address}
+            onChange={e => setAddress(e.target.value)}
+          />
         </Form.Group>
 
-
-
-
-        <Button variant="primary" type="submit">
+        <Button variant='primary' type='submit'>
           Submit
         </Button>
       </Form>
-    )
-}
+    </>
+  );
+};
 
-
-export default CreateAccountForm
+export default CreateAccountForm;
