497704123cf677c44f768a3494d963accdc2b103
DNiev <danny.nieves411@gmail.com>
2021-04-08 22:25:11 -0400

    added logo and added a fetch to get current user information for the profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,18 +1,36 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import {useEffect, useState} from 'react'
 
-const Profile = () => {
+const Profile = (props) => {
+  const[userInfo, setUserInfo] = useState(null)
+  const getUserInfo = () => {
+    fetch('./api/users/me', {'credentials': 'include'})
+    .then(res => res.json())
+    .then(res => {
+      setUserInfo(res)
+      props.setUserLoginIn(true)
+    })
+    .catch(e => {
+      console.log(e)
+    })
+  }
+  useEffect(() => {
+    getUserInfo()
+  }, [])
+
   return (
     <div>
+      <img src={require('../assets/logo.svg').default} alt="pies and fries logo" className="profile-logo"/>
       <div>
-        <h1>User Name</h1>
-        <h3>email</h3>
+        <h1>{userInfo.name}</h1>
+        <h3>{userInfo.email}}</h3>
       </div>
       <hr />
       <div>
         <h1>Profile</h1>
         <ul>
-          <li>Perosnal Info</li>
+          <li>Personal Info</li>
           <li>Payment Options</li>
           <li>Transaction History</li>
         </ul>
