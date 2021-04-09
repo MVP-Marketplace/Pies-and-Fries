ba62741364cc40a4e1a8e39700406d8559a7ca2d
Jorge <jorgesilva1892@gmail.com>
2021-04-09 14:01:09 -0700

    hamburger menu add .

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Cart from './Cart';
+import HamburgerMenu from './HamburgerMenu';
 
 const Navbar = props => {
   const logoutUser = () => {
@@ -17,40 +18,43 @@ const Navbar = props => {
       });
   };
   return (
-    <div className='Navbar'>
-      <div className='navLogo'>
-        <Link to='/'>
-          <button type='button' className='logo'>
-            Logo
-          </button>
-        </Link>
-      </div>
-      <div className='navOptions'>
-        {props.userLoggedIn && (
-          <Link to='/dashboard' className='navItem'>
-            Dashboard
+    <>
+      <HamburgerMenu />
+      <div className='Navbar'>
+        <div className='navLogo'>
+          <Link to='/'>
+            <button type='button' className='logo'>
+              Logo
+            </button>
           </Link>
-        )}
+        </div>
+        <div className='navOptions'>
+          {props.userLoggedIn && (
+            <Link to='/dashboard' className='navItem'>
+              Dashboard
+            </Link>
+          )}
 
-        {/* <Link to ="/store" className="navItem">Store</Link> */}
+          {/* <Link to ="/store" className="navItem">Store</Link> */}
 
-        {props.userLoggedIn ? (
-          <Link
-            to='/signin'
-            className='navItem-signin'
-            onClick={() => logoutUser()}
-          >
-            Logout
-          </Link>
-        ) : (
-          <Link to='/signin' className='navItem-sigin'>
-            Sign In
-          </Link>
-        )}
-        {props.cartLength}
+          {props.userLoggedIn ? (
+            <Link
+              to='/signin'
+              className='navItem-signin'
+              onClick={() => logoutUser()}
+            >
+              Logout
+            </Link>
+          ) : (
+            <Link to='/signin' className='navItem-sigin'>
+              Sign In
+            </Link>
+          )}
+          {props.cartLength}
+        </div>
+        <Cart />
       </div>
-      <Cart />
-    </div>
+    </>
   );
 };
 
