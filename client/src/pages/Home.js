054c7e96139ff9fc645a210f5aebd11b0ef16992
tracilevine <televine@gmail.com>
2021-04-30 15:39:09 -0400

    edits

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import pizzaMonster from '../assets/pizzamonster.svg';
-import pizzaPaddle from '../assets/pizzaPaddle.svg'
-import '../styles/Home.css';
+import pizzaPaddle from '../assets/pizzaPaddle.png';
+import pizzaHero from '../assets/pizzaHero.png';
+// import '../styles/Home.css';
 
 const home = () => {
   return (
@@ -14,43 +15,29 @@ const home = () => {
       </div>
       <div className='container-midsection'>
         <div className='midsection-left'>
+          <div className='midsection-middle-text'>
           <p className='midsection-des-1'>Welcome Bonus!</p>
           <p className='midsection-des-2'>
             10% off on your first order and free delivery !!
           </p>
+          </div>
+          
+          <img src={pizzaHero} alt="pizza" id="pizza-img"/>
         </div>
         <div className='midsection-right'>
-          <div className="midsection-top">
+    
           <div className='midsection-middle-text'>
             <p className='midsection-des-2'>Earn our</p>
             <p className='midsection-des-1'>Rewards!</p>
             <p className='midsection-des-2'>Are you hungry?</p>
           </div>
           <div>
-            <img src={pizzaMonster} alt="pizza monster"/>
+            <img src={pizzaMonster} alt="pizza monster" />
           </div>
           </div>
-          <button className="midsection-btn" id="right-btn">See More+</button>
-        </div>
       </div>
-      <div className="hungry-cont">
-        <div className="pizza-des-cont">
-          <p className="heading2">Classic Cheese Pizza</p>
-          <button className="midsection-btn">Details+</button>
-          <div>
-          <p>Delivery</p>
-          <p>30 minutes</p>
-          <div>
-            <p>4.0</p>
-            <img src="" alt="stars" />
-            <button className="hungry-order-btn">Order Now</button>
-          </div>
+      <div className='order-section-container'>
 
-          </div>
-        </div>
-        <div>
-          <img src={pizzaPaddle} alt="pizza"/>
-        </div>
       </div>
     </div>
   );
