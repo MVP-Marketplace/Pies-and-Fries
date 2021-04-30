da735f897d9764fed88698d580d57e876de02ea3
tracilevine <televine@gmail.com>
2021-04-29 23:33:05 -0400

    started styling

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,26 +1,17 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import pizzaMonster from '../assets/pizzamonster.svg';
+import pizzaPaddle from '../assets/pizzaPaddle.svg'
 import '../styles/Home.css';
 
 const home = () => {
   return (
     <div className='hompage-container'>
-      <h1 className='heading-homepage'>Shalom, Friend!</h1>
       <div className='box1'>
-        <p className='box-1-des'>BEST pizza in the FASTEST way!</p>
-        <p className='box-1-des-2'>
-          Pizza ordering has never been so easy and simple. Enjoy one click
-          pizza ordering experience with Pies & Fries More you order - more free
-          pies you earn!
-        </p>
+        <h1 className='heading-homepage'>Shalom, Friend!</h1>
+        <p className='box-1-des'>ONE click away to your Pizza!</p>
+        <p className='box-1-des'>Earn rewards as you go!</p>
       </div>
-
-      <div className='hungry-cont'>
-        <h2 className='heading2'>Are You HUNGRY?!</h2>
-        <button className='hungry-order-btn'>Order Now</button>
-      </div>
-
       <div className='container-midsection'>
         <div className='midsection-left'>
           <p className='midsection-des-1'>Welcome Bonus!</p>
@@ -28,7 +19,7 @@ const home = () => {
             10% off on your first order and free delivery !!
           </p>
         </div>
-        <div className='midsection-middle'>
+        <div className='midsection-right'>
           <div className="midsection-top">
           <div className='midsection-middle-text'>
             <p className='midsection-des-2'>Earn our</p>
@@ -39,6 +30,26 @@ const home = () => {
             <img src={pizzaMonster} alt="pizza monster"/>
           </div>
           </div>
+          <button className="midsection-btn" id="right-btn">See More+</button>
+        </div>
+      </div>
+      <div className="hungry-cont">
+        <div className="pizza-des-cont">
+          <p className="heading2">Classic Cheese Pizza</p>
+          <button className="midsection-btn">Details+</button>
+          <div>
+          <p>Delivery</p>
+          <p>30 minutes</p>
+          <div>
+            <p>4.0</p>
+            <img src="" alt="stars" />
+            <button className="hungry-order-btn">Order Now</button>
+          </div>
+
+          </div>
+        </div>
+        <div>
+          <img src={pizzaPaddle} alt="pizza"/>
         </div>
       </div>
     </div>
