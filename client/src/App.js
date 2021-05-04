f50c0530b6aceaa44f77e4558fa6a3654a7fae59
Ryan Lugtu <ryanlugtu@gmail.com>
2021-05-04 15:36:34 -0400

    added redirect when admin logs in

--- client/src/App.js
+++ client/src/App.js
@@ -45,6 +45,9 @@ function App() {
       <Route exact path = '/'>
         {user && user.driver ? <Redirect to="/driver"/> : <Redirect to="/" />}
       </Route>
+      <Route exact path = '/'>
+        {user && user.admin ? <Redirect to="/admin"/> : <Redirect to="/" />}
+      </Route>
       <Route exact path='/' component={Home} />
       
 
@@ -52,8 +55,13 @@ function App() {
 
       <Route exact path='/tracking' render={() => <Tracking />} />
       <Route exact path='/signup' render={() => <SignIn signIn={false} />} />
-      <Route exact path='/admin' render={() => <Admin />} />
+      
+      <Route exact path='/admin' render={() => <Admin />} /> 
+      {/* {user && user.admin === true ?  <Route exact path='/admin' render={() => <Admin />} /> : <Redirect to="/" />} */}
+    
       {user && user.driver ? <Route exact path='/driver' render={() => <Driver />} /> : <Redirect to="/" />}
+      {user && user.admin ? <Route exact path='/admin' render={() => <Admin />} /> : <Redirect to="/" />}
+
     
       <Route exact path='/dashboard' render={() => <UserDashboard />} />
       <Route exact path='/store' render={() => <Store />} />
