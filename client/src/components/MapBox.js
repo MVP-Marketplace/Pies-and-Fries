81489f09094309059d4143860007d9d950218547
Jorge <jorgesilva1892@gmail.com>
2021-05-10 11:38:40 -0700

    adding some styles to the hm

--- client/src/components/MapBox.js
+++ client/src/components/MapBox.js
@@ -1,42 +1,36 @@
 import ReactMapboxGl, { Marker, ZoomControl, Popup } from 'react-mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import'../styles/DriverOrderDetails.css'
-
+import '../styles/DriverOrderDetails.css';
 
 const MapBox = new ReactMapboxGl({
-    accessToken: process.env.REACT_APP_MAPBOX_TOKEN
-  });
+  accessToken: process.env.REACT_APP_MAPBOX_TOKEN,
+});
 
-
-const Map = ({lng,lat}) => {
-
+const Map = ({ lng, lat }) => {
   return (
-        <div className="mapBox" id="map">
-            <MapBox
-                style="mapbox://styles/mapbox/streets-v9"
-                center={[lng, lat]}
-                container="map"
-                containerStyle={{
-                    width: '100vw',
-                    zoom: 15
-                  }}
-                
-                >
-                
-                
-                <Marker coordinates={[lng, lat]} className="marker">
-                    <img
-                    className="marker-icon"
-                    src={require('../assets/pizzamonster.svg').default}
-                    />
-                </Marker>
-                <ZoomControl />
-            </MapBox>
-        </div>
-        // <div>
-        //   <div ref={mapContainer} className="map-container" />
-        // </div>
-    
+    <div className='mapBox' id='map'>
+      <MapBox
+        style='mapbox://styles/mapbox/streets-v9'
+        center={[lng, lat]}
+        container='map'
+        containerStyle={{
+          width: '100vw',
+          zoom: 2,
+        }}
+      >
+        <Marker coordinates={[lng, lat]} className='marker'>
+          <img
+            className='marker-icon'
+            src={require('../assets/pizzamonster.svg').default}
+            alt='pizza-monster'
+          />
+        </Marker>
+        <ZoomControl />
+      </MapBox>
+    </div>
+    // <div>
+    //   <div ref={mapContainer} className="map-container" />
+    // </div>
   );
 };
 export default Map;
